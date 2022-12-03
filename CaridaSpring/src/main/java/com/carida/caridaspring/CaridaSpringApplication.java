348f9e0944d9186a81ba900e1d3e62accfa428d1
jesu-m0 <jesusmd2000@gmail.com>
2022-12-03 11:31:47 +0100

    Some files

---
+++ CaridaSpring/src/main/java/com/carida/caridaspring/CaridaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.carida.caridaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaridaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaridaSpringApplication.class, args);
+    }
+
+}
---
+++ CaridaSpring/src/test/java/com/carida/caridaspring/CaridaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carida.caridaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaridaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
