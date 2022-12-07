75a2cf681d84ee1cc5e1e14ba891e7d7c5e79e35
jesu-m0 <jesusmd2000@gmail.com>
2022-12-07 15:31:44 +0100

    Project structure added and bootstrap

--- CaridaAngular/src/app/app.module.ts
+++ CaridaAngular/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavarComponent } from './components/simpleComponents/navar/navar.component';
+import { IndexPageComponent } from './components/templates/IndexPage/IndexPage.component';
+import { FooterComponent } from './components/simpleComponents/footer/footer.component';
+
+const appRoutes: Routes=[
+
+  {path:'', component: IndexPageComponent}
+
+
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexPageComponent,
+    NavarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ CaridaAngular/src/app/components/templates/IndexPage/IndexPage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-IndexPage',
+  templateUrl: './IndexPage.component.html',
+  styleUrls: ['./IndexPage.component.css']
+})
+export class IndexPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
