bfb8a212c5dfca7013ac9f4a3919c9fb09b6d5b7
Luis Miguel García <linten42@gmail.com>
2022-12-09 12:09:56 +0100

    Add Initial Point Mark selector

--- CaridaAngular/src/app/components/templates/newTripPage/newTripPage.component.ts
+++ CaridaAngular/src/app/components/templates/newTripPage/newTripPage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+//import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-newTripPage',
@@ -10,6 +11,41 @@ export class NewTripPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  /*
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow | undefined;
+
+  center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
+  markerPosition?: google.maps.LatLngLiteral;
+  zoom = 4;
+  display: any;
+
+  addMarker(event: google.maps.MapMouseEvent) {
+    if(event.latLng != null)
+    this.markerPosition = event.latLng.toJSON();
+  }
+
+  openInfoWindow(marker: MapMarker) {
+    if(this.infoWindow != undefined)
+    this.infoWindow.open(marker);
+    this.display = marker.getPosition()?.toJSON();
+  }
+  */
+
+  public onOpenModal(mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-bs-toggle', 'modal');
+    if (mode === 'initial') {
+      button.setAttribute('data-bs-target', '#addInitialMarkPointModal');
+    } 
+    if (mode === 'destination') {
+      button.setAttribute('data-bs-target', '#addDestinationMarkPointModal');
+    }
+    container?.appendChild(button);
+    button.click();
   }
 
 }
